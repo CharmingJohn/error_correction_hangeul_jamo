92dc0e693958663aff8bb8f5e93e39be2f912ffd
CharmingJohn <45421181+CharmingJohn@users.noreply.github.com>
2021-03-11 13:27:14 +0900

    Add files via upload

---
+++ error_correction_polbot.py
@@ -0,0 +1,51 @@
+import difflib
+from functools import reduce
+
+ja = 'ㄱㄲㄴㄷㄸㄹㅁㅂㅃㅅㅆㅇㅈㅉㅊㅋㅍㅎ#'
+mo = 'ㅏㅐㅑㅐㅓㅔㅕㅖㅗㅘㅙㅚㅛㅜㅝㅞㅟㅠㅡㅢㅣ#'
+bad = ' ㄱㄲㄳㄴㄵㄶㄷㄹㄺㄻㄼㄽㄾㄻㅀㅁㅂㅄㅅㅆㅇㅈㅊㅋㅌㅍㅎ#'
+
+BASE = 0xAC00
+
+def segment(ch):
+
+    code = ord(ch) - BASE
+    jong = code % 28
+
+    code = code - jong
+    jung = int((code / 28) % 21)
+
+    code = int(code / 28)
+    cho = int(code / 21)
+
+    if cho < 0 :
+        cho = -1
+    if jong > 19 :
+        jong = -1
+
+    return ja[cho], mo[jung], bad[jong]
+
+def diff(word1, word2):
+    L1 = ''.join(reduce(lambda x1, x2 : x1+x2, map(segment, word1)))
+    L2 = ''.join(reduce(lambda x1, x2 : x1+x2, map(segment, word2)))
+    differ = difflib.SequenceMatcher(None, L1, L2)
+    return differ.ratio()
+
+def correction(call):
+    #단어 세트 추가 및 변경
+    target = ['상담사', '신호', '속도', '과속', '범칙금']
+
+    result = 0
+    result_word = ''
+
+    for i in range(len(target)):
+        current = diff(call, target[i])
+        current_word = target[i]
+        if current > result:
+            result = current
+            result_word = current_word
+
+    return result_word
+
+if __name__ == "__main__":
+    print(correction('신우'))
---
+++ test.py
@@ -0,0 +1,3 @@
+import error_correction_polbot
+
+print(error_correction_polbot.correction('가속'))
