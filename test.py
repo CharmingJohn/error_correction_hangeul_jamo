010a4c1a0a91e614c692347eda12439563ec8911
CharmingJohn <45421181+CharmingJohn@users.noreply.github.com>
2021-03-11 16:05:27 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,12 @@
-import error_correction_polbot
+# -*- coding: utf-8 -*-
 
-print(error_correction_polbot.correction('가속'))
+import error_correction_polbot
+import argparse
+
+parser = argparse.ArgumentParser(description='mj')
+
+parser.add_argument('--word', required = True, help = '단어 입력')
+
+args = parser.parse_args()
+
+print(error_correction_polbot.correction(arg.word))
